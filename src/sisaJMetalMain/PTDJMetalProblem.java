143196ecc4b506edf5b3f2b5317049f4daf199f4
Jorge Candeias do Nascimento <elntele@gmail.com>
2016-11-02 14:18:58 -0200

    metodos criados em PDTJmetal deletados

--- src/sisaJMetalMain/PTDJMetalProblem.java
+++ src/sisaJMetalMain/PTDJMetalProblem.java
@@ -19,69 +19,7 @@ public class PTDJMetalProblem extends AbstractIntegerProblem {
 		bonsProblem = new OpticalNetworkProblem(networkLoad, gmlFile);
 	}
 	
-	/*// variacia do periodo
-		public void VarianciaPeriodo(){
-			float varia=0;
-			ArrayList<Float>  minilist= new ArrayList<Float>();// um buffer pra lista
-			//private List <Float> VarianciaPeriodo=new ArrayList<Float>();
-
-			int cont=0, deivisorPorPeriodo=0;
-			for(Disciplina D : this.sugeSobAvaliacao.getGradeDeHorarios()) {// percorre o array de disciplinas para calcular a variancia por periodo
-				cont+=1;
-				if (D.getNome()!="tampabrecha"){ // se não houver disciplina na posição
-					deivisorPorPeriodo+=1;		// a estratégia é criar uma disciplina 
-					}							// com esse nome	
-				varia += (float) Math.pow( D.getGrauDificuldade()- this.sugeSobAvaliacao.getMedia(),2);
-				if (cont%8==0){
-					if (deivisorPorPeriodo!=0){
-						
-						minilist = (ArrayList<Float>) this.sugeSobAvaliacao.getVarianciaPeriodo();
-						minilist.add(varia/deivisorPorPeriodo);
-						this.sugeSobAvaliacao.setVarianciaPeriodo(minilist);
-						cont=0;
-					 }else{
-						 minilist.add((float) 0);
-						 this.sugeSobAvaliacao.setVarianciaPeriodo(minilist);
-					}
-				}
-			}
-			
-		}
-
-		
-		//metodo pra contar quantas disciplinas de cada area
-		public void contadorDeArea(){
-			int buffer;
-			for(Disciplina D : this.sugeSobAvaliacao.getGradeDeHorarios()){
-				switch (D.getArea().getNome()){
-					case "ensiso": buffer = this.sugeSobAvaliacao.getContadorAreaEnsiso();
-								   buffer+=1;
-								   this.sugeSobAvaliacao.setContadorAreaEnsiso(buffer);
-									break;
-					case "Arq":buffer = this.sugeSobAvaliacao.getContadorAreaArq();
-					   		   buffer+=1;
-					   		   this.sugeSobAvaliacao.setContadorAreaArq(buffer);
-					   		   break;
-					case "FC": buffer = this.sugeSobAvaliacao.getContadorAreaFC();
-		   					   buffer+=1;
-		   					   this.sugeSobAvaliacao.setContadorAreaFC(buffer);
-		   					   break;
-					default: break;					
-				}
-			}	
-		}
-		// compia da sugestão apenas uam lista com os códigos
-	private void setaLista(){
-		ArrayList<Integer>  minilist2= new ArrayList<Integer>();
-		for(Disciplina D : this.sugeSobAvaliacao.getGradeDeHorarios()){
-			minilist2=(ArrayList<Integer>) this.sugeSobAvaliacao.getGradeHorarioSoCodigoDaDisciplina();
-			minilist2.add(D.getCodigo());
-			this.sugeSobAvaliacao.setGradeHorarioSoCodigoDaDisciplina(minilist2);
-		}
-
-		
-	}
-*/
+	
 	@Override
 	public void evaluate(IntegerSolution solution) {
 		Integer[] vars = new Integer[solution.getNumberOfVariables()];
